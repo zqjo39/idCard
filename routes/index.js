b2a43096470efcf968dec7cac7dcdd3433333966
zqjo <zqjo@fresnou.org>
2022-10-11 11:39:43 -0700

    initial commit!

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.post('/card', function(req, res) {
+  console.log(req.body.dateOfBirth)
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    addressLineOne: req.body.addressLineOne,
+    addressLineTwo: req.body.addressLineTwo,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: createAccountNumber(),
+    currentDate: new Date()
+  })
+});
+
+module.exports = router;
+
+function createAccountNumber() {
+  let accNum = ""
+  for (let i = 0; i < 5; i++) {
+    let temp = Math.floor(Math.random() * 10);
+    accNum += temp;
+  }
+  return accNum;
+}
